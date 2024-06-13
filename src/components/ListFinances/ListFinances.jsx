67ff6b38caa9f1b2bbbf2b8e6853ec180f124f75
Tatiana Gutierrez <tg5853523@gmail.com>
2024-06-13 11:19:53 -0500

    orin

--- src/components/ListFinances/ListFinances.jsx
+++ src/components/ListFinances/ListFinances.jsx
@@ -1,7 +1,3 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-export const ListFinances = ({content, route}) => {
-        return (
-          <li><NavLink  to={route} >{content}</NavLink></li>
-        )
-    }
+
+export const ListFinances = ({children}) => <li>{children}</li>
